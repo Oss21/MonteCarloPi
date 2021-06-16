9cda81f9ca53e7a67be2b416017d8e27f641b947
ZornStolz <juan.vera1@correo.icesi.edu.co>
2021-06-16 03:59:15 -0500

    lectura y escritura de test. falta timestamp

--- client/src/main/ClientImp.java
+++ client/src/main/ClientImp.java
@@ -2,6 +2,11 @@ package main;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 
 import org.osoa.sca.annotations.Reference;
 
@@ -11,6 +16,10 @@ import services.ServiceClient;
 public class ClientImp implements Runnable, ServiceClient {
 
 	private static GUI gui;
+	
+	private static ArrayList<String[]> datos;
+	
+	private static String ruta = "./data/";
 
 	@Reference
 	private ServiceBroker serviceBroker;
@@ -38,6 +47,27 @@ public class ClientImp implements Runnable, ServiceClient {
 	public double calcularPi(double puntosCirculo, double puntosCuadrado) {
 		return 4 * (puntosCirculo / puntosCuadrado);
 	}
+	
+	public void cargarDatos(){
+		
+		datos = new ArrayList<String[]>();
+		
+		String[] arreglo = null;
+		String line = "";
+		
+		try {
+			FileReader reader = new FileReader(ruta + "test_easy.txt"); //cambiar por test
+			BufferedReader br = new BufferedReader(reader);
+			
+			while((line = br.readLine()) != null){
+				arreglo = line.split(",");
+				datos.add(arreglo);			
+			}
+			br.close();
+		} catch (Exception exc) {
+			
+		}			
+	}
 
 	private void configureEvents() {
 		gui.getButCalcPi().addActionListener(new ActionListener() {
@@ -49,6 +79,50 @@ public class ClientImp implements Runnable, ServiceClient {
 				double[] values = serviceBroker.darPuntos(semilla, numPuntos);
 				double pi = calcularPi(values[0], values[1]);
 				gui.setLabValPi(pi);
+			}
+		});
+		
+		gui.getButCargarPrueba().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int	rep = 10; //cantidad de veces que se va a realizar cada configuracion
+				cargarDatos(); 
+				int k = datos.size(); // num de configuraciones
+				
+				long demoraTotal = 0;
+				
+				// para guardar los resultados
+				try {
+					FileWriter writer = new FileWriter(ruta + "resultado.txt");
+					BufferedWriter bw = new BufferedWriter(writer);
+					
+					for (int i = 0; i < k; i++) {
+						String[] configuracion = datos.get(i);
+						
+						bw.write("Semilla: " + configuracion[0] + ", Cantidad de puntos: " + configuracion[1]);
+						
+						for (int j = 0; j < rep; j++) {
+							
+							//timestamp
+							
+							double[] values = serviceBroker.darPuntos(Long.parseLong(configuracion[0]), Double.parseDouble(configuracion[1]));
+							double pi = calcularPi(values[0], values[1]);
+							
+							//fin time stamp
+							
+							demoraTotal += 0; //timestamp
+							
+							bw.write("pi = " + pi + ", Demoro: ");
+						}							
+						bw.write("El promedio para esta configuración fue: " + (demoraTotal/rep));
+						demoraTotal =0;
+					}
+					
+					bw.close();
+				} catch (Exception exc) {
+					
+				}
+					
 			}
 		});
 	}
